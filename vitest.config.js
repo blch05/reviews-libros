3404f792286796514ac4f559e4c8a50b7d3ec35f
blch05 <blchristian2005@gmail.com>
2025-09-03 17:34:33 -0300

    fixes

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,10 +7,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     globals: true,
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
   },
 })
